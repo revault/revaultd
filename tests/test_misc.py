7d5bc09a259bcc06c1c21cdadcc031fe69a45603
Antoine Poinsot <darosior@protonmail.com>
2020-11-08 23:34:35 +0100

    tests: add a revaultd_manager fixture and test startup
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,7 +1,12 @@
 from fixtures import (
-    revaultd_stakeholder, bitcoind, directory, test_base_dir, test_name
+    revaultd_stakeholder, revaultd_manager, bitcoind, directory, test_base_dir,
+    test_name
 )
 
 def test_revaultd_stakeholder_starts(revaultd_stakeholder):
+    # The fixture waits until "revaultd started" so we fine here
+    pass
+
+def test_revaultd_manager_starts(revaultd_manager):
     # The fixture waits until "revaultd started" so we fine here
     pass

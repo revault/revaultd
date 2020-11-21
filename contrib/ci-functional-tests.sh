012e56eb1504fca554709df2ffe0e5a342aeb4a9
Antoine Poinsot <darosior@protonmail.com>
2020-11-21 17:55:24 +0100

    ci: checkout v0.21 now it's been tagged
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- contrib/ci-functional-tests.sh
+++ contrib/ci-functional-tests.sh
@@ -6,14 +6,15 @@ sudo apt install python3 python3-pip python3-venv libsqlite3-dev build-essential
 
 # FIXME: dl it from bitcoincore.org post branch-off
 git clone https://github.com/bitcoin/bitcoin && cd bitcoin
+git checkout v0.21.0rc1
 ./contrib/install_db4.sh `pwd`
 ./autogen.sh
 export BDB_PREFIX="`pwd`/db4"
-./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --without-gui --disable-tests --enable-c++17
+./configure BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include" --without-gui --disable-tests --disable-bench --enable-c++17
 make -j4 && sudo make install
 cd ..
 
 python3 -m venv venv
 . venv/bin/activate
 pip install -r tests/requirements.txt
-pytest -vvv -n2 tests/
+TEST_DEBUG=1 pytest -vvv -n2 tests/

5a69cd6905ea852a8ff8a82d5ac0a44e5c27cc6f
Antoine Poinsot <darosior@protonmail.com>
2021-01-21 12:46:08 +0100

    contrib: update bitcoind to 0.21.0
    Signed-off-by: Antoine Poinsot <darosior@protonmail.com>

--- contrib/ci-functional-tests.sh
+++ contrib/ci-functional-tests.sh
@@ -4,10 +4,10 @@ set -xe
 cargo build --release
 
 # Download the bitcoind binary
-BITCOIND_VERSION="0.21.0rc2"
+BITCOIND_VERSION="0.21.0"
 DIR_NAME="bitcoin-$BITCOIND_VERSION"
 ARCHIVE_NAME="$DIR_NAME.tar.gz"
-curl https://bitcoincore.org/bin/bitcoin-core-0.21.0/test.rc2/bitcoin-0.21.0rc2-x86_64-linux-gnu.tar.gz -o $ARCHIVE_NAME
+curl https://bitcoincore.org/bin/bitcoin-core-$BITCOIND_VERSION/bitcoin-$BITCOIND_VERSION-x86_64-linux-gnu.tar.gz -o $ARCHIVE_NAME
 tar -xzf $ARCHIVE_NAME
 sudo mv $DIR_NAME/bin/bitcoind /usr/local/bin/
 
